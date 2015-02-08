d433127b71f1d5ef33c09034fd1f1230f749388d
AdamThePieRat <adamjc86@gmail.com>
2015-02-08 08:02:17 +0000

    big rework, moved everything inside constructor to namespace everything properly. Added tests.

--- hash-set.js
+++ hash-set.js
@@ -1,56 +1,111 @@
 // HashSet implementation in JavaScript.
 
-//
+// Internally uses an implementation of a hash map.
 
 var HashSet = (function () {
-    var _items = {};
+    function HashSet(items) {
+        /* private variables */
+        var _items = {};
 
-    function HashSet() {
-        this.addItem = addItem;
-        this.removeItem = removeItem;
-        this.generateHash = generateHash;
-        this.getItems = getItems;
-    }
+        /* public methods */
 
-    function getItems() {
-        return _items;
-    }
+        /**
+         * getAll - returns the set.
+         */
+        this.getAll = function() {
+            return _items;
+        };
 
-    function addItem(item) {
-        var key = generateHash(item);
+        /**
+         * add - adds an item to the hash set
+         *
+         * @param  {Object} item - the item to add
+         * @param  {String} keyId - the key of the object to use as a hash
+         * @return {Boolean} success or failure.
+         */
+        this.add = function(item, keyId) {
+            var key = hashCode(item[keyId]);
 
-        if (key !== undefined) {
-            _items[key] = item;
+            if (key !== undefined && _items[key] === undefined) {
+                _items[key] = item;
+                return true;
+            }
+
+            return false;
+        };
+
+        /**
+         * addAll - adds an array of items to the hash set
+         *
+         * @param  {Array} items - the items to add
+         * @param  {String} keyId - the key of the objects to use as a hash
+         */
+        this.addAll = function(items, keyId) {
+            for (var i = 0; i < items.length; i++) {
+                var isAdded = this.add(items[i], keyId);
+            }
+        };
+
+        /**
+         * remove - removes an item from the hash set
+         *
+         * @param  {Object} item - the item to remove
+         * @param  {String} keyId - the key of the object to use as a hash
+         * @return {Boolean} success or failure.
+         */
+        this.remove = function(item, keyId) {
+            var key = hashCode(item[keyId]);
+
+            if (key !== undefined) {
+                delete _items[keyId];
+                return true;
+            }
+
+            return false;
+        };
+
+        /**
+         * size - returns the size of the hash set.
+         *
+         * @return {int} how many items are in the hash set.
+         */
+        this.size = function() {
+            return Object.keys(_items).length;
+        };
+
+        /* private methods */
+
+        /**
+         * generateHash - tries to, within reason, generate a random key, this
+         * is not guaranteed to be unique.
+         *
+         * @return {int} hash - a (hopefully) unique hash.
+         */
+        function hashCode(key) {
+            var hash = 0;
+            var stringKey = key.toString();
+
+            if (stringKey.length === 0) return hash;
+
+            for (i = 0; i < stringKey.length; i++) {
+               char = stringKey.charCodeAt(i);
+
+               hash = ((hash << 5) - hash) + char;
+
+               // Convert to 32bit integer
+               hash = hash & hash;
+            }
+
+            return hash;
         }
-    }
 
-    function removeItem(item) {
-        var key = generateHash(item);
-
-        if (key !== undefined) {
-            delete _items[key];
+        /* constructor initialise */
+        if (items instanceof Array) {
+            this.addAll(items);
         }
-    }
-
-    function generateHash(item) {
-        var hash = 0;
-        var char;
-        var itemKey = item.toString();
-
-        if (itemKey.length === 0) {
-            return undefined;
-        }
-
-        for (var i = 0; i < itemKey.length; i++) {
-            char = itemKey.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-
-            // Convert to 32bit integer
-            hash |= 0;
-        }
-
-        return hash;
     }
 
     return HashSet;
 }());
+
+module.exports = HashSet;
---
+++ tests/hash-set.js
@@ -0,0 +1,65 @@
+var chai = require('chai');
+var should = chai.should();
+var HashSet = require('../hash-set');
+
+describe('hash-set', function () {
+    var hashSet;
+    var exampleItem;
+
+    beforeEach(function () {
+        hashSet = new HashSet();
+
+        exampleItem = {
+            _id: "valueOne",
+            someVal: "valueTwo",
+            someOtherVal: "valueThree"
+        };
+    });
+
+    describe('add(item)', function () {
+        it('should add an object to the hash set', function () {
+            hashSet.add(exampleItem, "_id").should.equal(true);
+        });
+
+        it('should not add duplicate objects', function () {
+            hashSet.add(exampleItem, "_id").should.equal(true);
+            hashSet.add(exampleItem, "_id").should.equal(false);
+        });
+    });
+
+    describe('addAll(items)', function () {
+        it('should add an array of objects to the hash set', function () {
+            var items = [{
+                _id: 1,
+                a: 1
+            }, {
+                _id: 2,
+                b: 2
+            }, {
+                _id: 3,
+                c: 3
+            }];
+
+            hashSet.addAll(items, "_id");
+
+            hashSet.size().should.equal(3);
+        });
+    });
+
+    describe('remove(item)', function () {
+        it('should remove the item from the set', function () {
+            hashSet.add(exampleItem, "_id");
+
+            hashSet.remove(exampleItem, "_id").should.equal(true);
+        });
+    });
+
+    describe('size()', function () {
+        it('should return the size of the hash set', function () {
+            hashSet.size().should.equal(0);
+
+            hashSet.add(exampleItem, "_id");
+            hashSet.size().should.equal(1);
+        });
+    });
+});
